80f60a7b591e031c619810236c087a4b01370b95
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2024-03-11 17:05:51 +0000

    add more article info into article card

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,14 +1,29 @@
 import { Link } from "react-router-dom";
-import { shortenTitle } from "../utils/text";
+import { formatDateTime, shortenTitle } from "../utils/text";
 
 function ArticleCard({ article }) {
-    const { article_id, title, article_img_url } = article;
+    const {
+        article_id,
+        title,
+        article_img_url,
+        author,
+        comment_count,
+        created_at,
+        votes
+    } = article;
 
     return (
         <Link to={`/articles/${article_id}`}>
             <section className="article-card">
                 <img src={article_img_url} alt={`article titled ${title}`} />
-                <div>{shortenTitle(title)}</div>
+                <div className="article-card-title">{shortenTitle(title)}</div>
+                <div className="article-card-left">{formatDateTime(created_at)}</div>
+                <div className="horizontal-text">
+                    <div className="horizontal-left">{author}</div>
+                    <div className="horizontal-right">
+                        {votes} votes | {comment_count} comments
+                    </div>
+                </div>
             </section>
         </Link>
     );
