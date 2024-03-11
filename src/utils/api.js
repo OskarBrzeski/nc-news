f277d0f72dec2348771f01ed53709a00ae024f73
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2024-03-11 15:51:42 +0000

    create components for viewing single article

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,3 +5,7 @@ const api = axios.create({ baseURL: "https://nc-news-moag.onrender.com/api" });
 export function getArticles() {
     return api.get("/articles");
 }
+
+export function getArticleById(article_id) {
+    return api.get(`/articles/${article_id}`);
+}
