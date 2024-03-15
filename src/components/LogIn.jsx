e6cc5022a6be9ceedef20351254bccd7d4afe573
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2024-03-15 15:46:01 +0000

    add error message when no user with username found

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router-dom";
 
 function LogIn() {
     const [username, setUsername] = useState("");
+    const [errorMsg, setErrorMsg] = useState("");
+    const [disabledButton, setDisabledButton] = useState(false);
 
     const { setUser } = useContext(UserContext);
 
@@ -16,6 +18,9 @@ function LogIn() {
 
     function onSubmit(event) {
         event.preventDefault();
+        
+        setErrorMsg("");
+        setDisabledButton(true);
 
         getUser(username)
             .then(() => {
@@ -24,13 +29,21 @@ function LogIn() {
                 navigate(`/user`);
             })
             .catch((error) => {
+                if (error.response) {
+                    setErrorMsg(`${username} does not exist`);
+                }
                 console.log(error);
+            })
+            .finally(() => {
+                setDisabledButton(false);
             });
     }
 
     return (
         <form className="sign-in-form" onSubmit={onSubmit}>
-            <label htmlFor="username"><h2>Sign in with your Username</h2></label>
+            <label htmlFor="username">
+                <h2>Sign in with your Username</h2>
+            </label>
             <input
                 type="text"
                 id="username"
@@ -38,7 +51,11 @@ function LogIn() {
                 onChange={onChange}
             />
 
-            <button type="submit">Sign in</button>
+            <button disabled={disabledButton} type="submit">
+                Sign in
+            </button>
+
+            <p>{errorMsg}</p>
         </form>
     );
 }
